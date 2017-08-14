3945be6ee4708a9c3a3102fac2e608458faa8e92
Werner Hofstra <wernerhofstra@gmail.com>
2017-08-14 17:20:55 +0200

    Add default linear interval and enforce jitter spread between 0 and 1

--- retry.py
+++ retry.py
@@ -16,6 +16,12 @@ MAX_ATTEMPTS = 10
 def make_jitterer(spread=0.01, rng=random):
     def jitter(interval):
         '''Applies some random jitter'''
+        if not 0 < spread < 1:
+            raise ArgumentError('Jitter spread should be between 0 and 1')
+
+        # Skip random number generation when there's no spread
+        if spread == 0:
+            return interval
         rand_number = rng.random()*2 - 1
         return interval + rand_number * interval * spread
 
@@ -79,7 +85,7 @@ class Backoff(Strategy):
 
 
 class Linear(Strategy):
-    def __init__(self, interval=None, *args, **kwargs):
+    def __init__(self, interval=1, *args, **kwargs):
         super().__init__(*args, **kwargs)
         self.interval = interval
 
